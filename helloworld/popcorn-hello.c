7cc48649faa8a566f039184d15dcd7b33f2f9dff
Antonio Barbalace <abarbala@stevens.edu>
2019-10-19 18:29:27 -0400

    refactoring for makeing more sense of the tutorial

--- helloworld/popcorn-hello.c
+++ helloworld/popcorn-hello.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
-#include <unistd.h>
 
 #include "migrate.h"
 
 int main(int argc, char *argv[])
 {
-	printf("hello, world (node %d)\n", current_nid());
-
-	migrate((current_nid() ? 0 : 1), 0, 0);
-
 	printf("hello, world (node %d)\n", current_nid());
 
 	return 0;
