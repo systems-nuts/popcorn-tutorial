786b65a3223fdcd122a0b92e58b26ad6f9a3342c
Antonio Barbalace <abarbala@stevens.edu>
2019-10-19 18:20:10 -0400

    refactoring hello with tools

--- helloworld/popcorn-hello.c
+++ helloworld/popcorn-hello.c
@@ -1,25 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h>
-#define _SIG_MIGRATION=1
-void print_iteration()
-{
-	static int i = 0;
-	printf("[%d] iteration %d\n", gettid(), i++);
 
-}
+#include "migrate.h"
 
 int main(int argc, char *argv[])
 {
-	printf("total_act_nodes: %d\n",total_act_nodes());
-	if(!current_nid()) printf("we are in X86, we migrate to ARM \n");
-	else{ printf("we are in ARM, we migrate to X86 \n");}
-	int arch = current_nid();
-	int i = 0;
-	while(i < 50) {
-		print_iteration();
-		sleep(1);
-		i++;
-	}
+	printf("hello, world (node %d)\n", current_nid());
+
+	migrate((current_nid() ? 0 : 1), 0, 0);
+
+	printf("hello, world (node %d)\n", current_nid());
+
 	return 0;
 }
