dc00fe7d58bb8b38ac2a5e1894dbfbccf58667d4
Antonio Barbalace <abarbala@stevens.edu>
2019-10-21 16:00:59 -0400

    updated psync with password option
    updated helloworldN

--- helloworldN/popcorn-hello.c
+++ helloworldN/popcorn-hello.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <string.h>
+
 #include "migrate.h"
-void print_iteration()
-{
-	static int i = 0;
-	printf("[%d] iteration %d in node %d\n", gettid(), i++, current_nid());
-}
+
+#define N 4
 
 int main(int argc, char *argv[])
 {
-	printf("total_act_nodes: %d\n",total_act_nodes());
-	if(!current_nid()) printf("We are in X86, we migrate to ARM \n");
-	else{ printf("we are in ARM, we migrate to X86 \n");}
-	int i = 0;
-	while(i<=20) {
-		print_iteration();
-		sleep(1);
-		migrate(!current_nid(),NULL,NULL);
-		i++;
+	int i;
+
+	printf("hello, world (node %d)\n", current_nid());
+
+	for (i=0; i<N; i++) {
+		migrate((current_nid() ? 0 : 1), 0, 0);
+		printf("hello, world (node %d)\n", current_nid());
 	}
+
 	return 0;
 }
